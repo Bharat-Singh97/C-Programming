f7a8f4e645d051346c4978c10fb2b334d6e2718c
Bharat-Singh97 <bs290094@gmail.com>
2024-11-13 16:22:30 +0530

    Concept based on printf function

---
+++ Problem.c
@@ -0,0 +1,8 @@
+//What is x ans  is 6 complete output Bharat 6
+#include<stdio.h>
+
+int main() {                //rintf prints the string Bharat then its count number of characters and assign into x then finally its print bharat 6 valux of x is 6 
+int x = printf("Bharat");  // printf function jo bhi chij print karta hai vo return me number of character deta hai isiliye iska output bharat  6 hoga
+printf(" %d", x);
+return 0;
+}
