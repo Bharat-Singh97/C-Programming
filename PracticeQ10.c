fde6acc284addc6d97bb221147fe78a3b408934a
Bharat-Singh97 <bs290094@gmail.com>
2024-11-14 21:40:06 +0530

    add to find smallest number of two baes program

---
+++ PracticeQ10.c
@@ -0,0 +1,14 @@
+// Write a program to print smallest number of two
+#include<stdio.h>
+
+int main () {
+    int a , b;
+    printf("Enter a number a ,b :");
+    scanf(" %d%d", &a , &b);
+    
+    if(a<b)
+         printf(" a is smallest number :%d", a);
+    else 
+         printf("b is smallest : %d", b);
+    return 0;
+}
