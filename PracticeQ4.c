21183131993b3ad0089830cacc03c1cbca16ecb3
Bharat-Singh97 <bs290094@gmail.com>
2024-11-14 21:33:36 +0530

    ADD type conversion based program

---
+++ PracticeQ4.c
@@ -0,0 +1,8 @@
+//types conversion
+#include<stdio.h>
+
+int main() {
+    int a =(int) 1.9999;    // explicit conversion means force fully convert float to int compiler self not convert higher to loewr  its remove decimal value
+ printf(" %d\n", a);
+ return 0; // output 1
+}
