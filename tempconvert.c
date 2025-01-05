2fa52e8303e9a6fac1ad4fbabf10ceb9066857c9
Bharat-Singh97 <119155310+Bharat-Singh97@users.noreply.github.com>
2025-01-05 22:08:06 +0530

    Add a program to convert temperature

--- tempconvert.c
+++ tempconvert.c
@@ -1,4 +1,4 @@
-/*Temperature Converter
+ /*Temperature Converter
 Write a C program that converts a temperature in Celsius to Fahrenheit. The Celsius temperature should be stored in a float variable celsius, and the Fahrenheit equivalent should be printed using a single printf statement. The relation between the two scales is given as
 
 fahrenheit = (celsius * 9/5) + 32;
